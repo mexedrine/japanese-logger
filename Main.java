386ed006254620ab35916ef7216fd5823e7b1f7f
mexedrine <qpfehlen@gmail.com>
2025-08-19 05:59:46 -0500

    help

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.time.*; // for handling dates, times, durations
+import java.io.*; // for file saving/loading
+import java.util.*; // for lists, maps, etc.
+import java.sql.*; // SQLite driver, use JSON or CSV for now.
+// option later: javafx.* → if you want a desktop GUI.
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public void Session() {
+
+    }
+
+    public void Management() {
+
+    }
+
+}
